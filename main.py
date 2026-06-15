2bde9b0b5713b59d9b13f9b20fc4d8c3936b4629
lonsdale228 <v.gerashchenkoua@gmail.com>
2026-06-15 14:19:50 +0300

    sensitivity

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sqlite3
+import pandas as pd
 
 
 def sql_query(name: str):
-    conn = sqlite3.connect('huge_db.db')
-    cursor = conn.cursor()
-    a = cursor.execute(f'SELECT * FROM users where name={name}')
-    print(a.fetchall())
-    conn.close()
+    df = pd.DataFrame({'price': [100, 200, 300], 'qty': [2, 5, 1]})
+
+    totals = []
+    for index, row in df.iterrows():
+        totals.append(row['price'] * row['qty'])
+    df['total'] = totals
 
 
 if __name__ == '__main__':
