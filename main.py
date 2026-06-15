be1e90e104b279815b13207291b7284d9455e6c8
lonsdale228 <v.gerashchenkoua@gmail.com>
2026-06-15 14:08:35 +0300

    sensitivity

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import sqlite3
-
+import requests
 
 def sql_query(name: str):
-    conn = sqlite3.connect('huge_db.db')
-    cursor = conn.cursor()
-    a = cursor.execute(f'SELECT * FROM users where name={name}')
-    print(a.fetchall())
-    conn.close()
-
+    token = "asdijuhweu:213132123sdjfksjfksdf"
+    response = requests.get(f"http://user.data.com/get_user/{token}")
+    print(response.json())
 
 if __name__ == '__main__':
     sql_query('Admin')
